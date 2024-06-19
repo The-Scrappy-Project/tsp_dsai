4e32c75b89afcf53bd2c3dc5adb2d6a66a8892b4
Rounak Banik <rounakbanik@gmail.com>
2024-06-19 21:16:06 +0530

    Minor changes

--- module_2/twentyQuestions.py
+++ module_2/twentyQuestions.py
@@ -17,20 +17,20 @@ You are a chatbot that is exceptional at playing the twenty questions game.
 
 # The first instruction that we give GPT
 prompt = """
-You are a chatbot that is exceptional at playing the twenty questions game, and guessing the name of the celebrity within twenty questions.
+You are a chatbot that is exceptional at playing the twenty questions game, and guessing the name of the movie within twenty questions.
 
-I have thought of a celebrity and your task is to guess who the celebrity is. You may do so by asking twenty or fewer questions. 
+I have thought of a movie and your task is to guess what the movie is. You may do so by asking twenty or fewer questions. 
 If you get it right, you stand a chance to win a thousand dollars.
 
 Here are a few rules
-- The celebrity I'm thinking of can be a Hollywood movie star or a singer
+- I'm thinking of a Hollywood movie.
 - You can only ask me yes or no questions. Alternatively, you can ask me a multiple choice question where I choose an option.
 - Assume I'm always telling the truth.
 - Assume if you get the answer right, I will tell you so. 
-- At no point of time am I allowed to change my celebrity midway.
+- At no point of time am I allowed to change my movie midway.
 - When asking a question, also let yourself and me know what question number it is. In other words, keep track of the number of questions you're asking.
 
-Me: I have thought of a celebrity. Ask me a question!
+Me: I have thought of a movie. Ask me a question!
 
 You:
 """
